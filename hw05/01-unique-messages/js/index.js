66c32c4672b05c1ba82e35bdbc26df322efbd8e3
mathiasnewman <36613472+mathiasnewman@users.noreply.github.com>
2019-05-12 12:47:00 -0500

    Adding my hw05 to the repo

---
+++ hw05/01-unique-messages/js/index.js
@@ -0,0 +1,14 @@
+const names = [
+    "Jane", "Brenda", "Wanda", "Maria", "Jasper",
+    "John", "Malik", "Arjun", "Larry", "Curly", "Moe"
+];
+
+
+// use a for / of loop to loop through each element
+// of the names array and output a message to the screen:
+for (name of names) {
+    document.write("Welcome, " + name + "! Make yourself comfortable.<br>");
+}
+// document.write("Welcome, " + names[0] + "! Make yourself comfortable.<br>");
+// document.write("Welcome, " + names[1] + "! Make yourself comfortable.<br>");
+// document.write("Welcome, " + names[2] + "! Make yourself comfortable.<br>");
---
+++ hw05/08-icon-variety/js/index.js
@@ -0,0 +1,26 @@
+const elem = document.getElementById('output')
+
+// Todo: make
+const classNames = [
+    'fa-heart',
+    'fa-beer',
+    'fa-star',
+    'fa-bicycle',
+    'fa-book',
+    'fa-apple-alt',
+    'fa-brain',
+    'fa-chess'
+]
+// Exercise: using a for / of loop, iterate through the classNames list
+// and output an icon that corresponds to each class.
+// Note that these icons are drawing from the FontAwesome icons:
+// https://fontawesome.com/icons?d=gallery&m=free
+for (className of classNames) {
+    elem.innerHTML += '<i class="fas ' + className + '"></i>'
+}
+
+/*
+elem.innerHTML += '<i class="fas ' + classNames[0] + '"></i>'
+elem.innerHTML += '<i class="fas ' + classNames[1] + '"></i>'
+elem.innerHTML += '<i class="fas ' + classNames[2] + '"></i>'
+*/
